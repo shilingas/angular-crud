37b4c23a6fc654e54d85ef915442791b0a51d2a7
shilingas <mariussilingas5@gmail.com>
2023-08-04 16:55:23 +0300

    whole cart adding sum functionality

--- AngularCrud/src/app/cart/cart.component.ts
+++ AngularCrud/src/app/cart/cart.component.ts
@@ -10,9 +10,11 @@ export class CartComponent {
   constructor(private cartService: CartService) { }
   cart!: Product[];
   itemCount: number = 0;
+  totalPrice: number = 0;
   ngOnInit() {
     this.cart = this.cartService.getCartItems();
     this.itemCount = this.cartService.getCartItems().length;
+    this.totalPrice = this.cartService.getCartItems().map((x:Product) => x.price).reduce((a: number,b: number) => a+b);
   }
   removeFromCart(product: Product) : void {
     this.cartService.removeItem(product);
